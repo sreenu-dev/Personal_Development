fbab37182dd70628f2b055ce2c694d4432390a4b
Sreenivasulu Ummadi <69182968+sreenu-dev@users.noreply.github.com>
2023-04-03 23:59:09 +0530

    Done Problems

---
+++ 111. Minimum Depth of Binary Tree/mdb.java
@@ -0,0 +1,11 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+}
+
+public class mdb {
+    public static void main(String args[]){
+        System.out.println("Working");
+    }
+}
---
+++ 9. Palindrome Number (Easy)/try_one.ts
@@ -0,0 +1,5 @@
+let x=121
+
+let data=x.toString()
+let rdata=x.toString().split('').reverse().join('')
+console.log(data===rdata?true:false)

45ed97b12c019cf1e0fe409255fcfca1c0bc4c0d
Sreenivasulu <ummadi.sreenivasulu@accenture.com>
2025-06-03 16:10:22 +0530

    Unwanted Files Deleted

--- React/Learn/MoneyManagementSystem/API/app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@localhost/moneymanagementsystem'
-    SQLALCHEMY_TRACK_MODIFICATION=False
--- React/Learn/my-first-app/src/Models/card.model.js
+++
@@ -1,7 +0,0 @@
-export class Card{
-    constructor(title, description, imageURL){
-        this.title = title;
-        this.description = description;
-        this.imageURL = imageURL;
-    }
-}
